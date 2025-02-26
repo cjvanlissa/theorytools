95a98752f8870d8f9ad76b61f5829faa3adb474e
cjvanlissa <c.j.vanlissa@tilburguniversity.edu>
2025-02-26 18:10:02 +0100

    update 2025-02-26 18:10:02.054153

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,41 @@
+#' @title Add Significance Asterisks
+#' @description Given a `data.frame` with a column containing p-values or two columns
+#' containing the lower- and upper bounds of a confidence interval, adds a
+#' column of significance asterisks.
+#' @param x A `data.frame`
+#' @param p_column Atomic character, referring to the name of the column of
+#' p-values. If this is provided, the confidence interval is ignored. Default:
+#' `NULL`
+#' @param ci_lb Atomic character, referring to the name of the column of
+#' the lower bound of a confidence interval. Default: `NULL`
+#' @param ci_up Atomic character, referring to the name of the column of
+#' the upper bound of a confidence interval. Default: `NULL`
+#' @param alpha Significance level, default: `.05`
+#' @return A `data.frame`
+#' @examples
+#' tmp <- add_significance(head(iris))
+#' @rdname add_significance
+#' @export
+add_significance <- function(x, p_column = NULL, ci_lb = NULL, ci_up = NULL, alpha = .05){
+  if(all(is.null(c(p_column, ci_lb, ci_up)))){
+    nams <- names(x)
+    if(sum(grepl("%", nams, fixed = TRUE)) == 2){
+      ci_lb <- grep("%", nams, fixed = TRUE, value = TRUE)[1]
+      ci_ub <- grep("%", nams, fixed = TRUE, value = TRUE)[2]
+    } else {
+      return(x)
+    }
+  }
+  if(!is.null(p_column)){
+    issig <- x[[p_column]] < alpha
+  } else {
+    issig <- !(rowSums(sign(x[, c(ci_lb, ci_ub)])) == 0)
+  }
+  x$significant <- c("", "*")[as.integer(issig)+1]
+  return(x)
+}
+
+
 path_rel <- function(fn, dn){
   unname(sapply(fn, function(the_fn){
     the_fn <- normalizePath(the_fn, winslash = .Platform$file.sep, mustWork = FALSE)
